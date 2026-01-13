ae8937b863d39393d850a170b0da4dc2fb24a579
NemesisX-ai <sameertanker@gmail.com>
2026-01-14 00:26:38 +0530

    changed

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <HashRouter basename="/pdf-hub">
+  <HashRouter>
     <App />
   </HashRouter>
 );
