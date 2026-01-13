90fa07e88e5fc6c5469b5b513ffedd7415aa07d6
NemesisX-ai <sameertanker@gmail.com>
2026-01-13 22:03:21 +0530

    hASH

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom"
+import { HashRouter } from "react-router-dom";
 
 import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <HashRouter>
-  <App />
-</HashRouter>
-
+  <HashRouter basename="/pdf-hub">
+    <App />
+  </HashRouter>
 );
